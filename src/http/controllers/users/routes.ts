501817c105b438d21679d6c997be2298b1eb006d
Anderson Coelho <anderson.coelho1969@gmail.com>
2025-09-05 20:02:10 -0400

    fix: organize users controller files on same directory

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,8 @@
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
-import { authenticate } from './controllers/authenticate'
-import { profile } from './controllers/profile'
-import { register } from './controllers/register'
-import { verifyJwt } from './middlewares/verify-jwt'
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { register } from './register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
